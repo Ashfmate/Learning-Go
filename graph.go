ff50785fb20f50ef0f0aef5c2c1394107a6b6fab
Ashfmate <ashforestunknown@gmail.com>
2024-02-22 23:11:40 +0300

    Transitions were made, using runes, next is refactoring into better code

--- graph.go
+++ graph.go
@@ -1,25 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"unicode/utf8"
+)
 
 type Node struct {
 	val   int
-	other []*Node
+	other map[rune]*Node
 }
 
-func traverse(start *Node) {
-	for _, val := range start.other {
-		traverse(val)
+func traverse(start *Node, input string) bool {
+	if start == nil {
+		return false
 	}
-	fmt.Println(start.val)
+	if start.val == 3 {
+		return len(input) == 0
+	}
+	if len(input) == 0 {
+		return false
+	}
+	symbol, width := utf8.DecodeRuneInString(input)
+	if len(start.other) == 0 {
+		return false
+	}
+	next := start.other[symbol]
+	return traverse(next, input[width:])
 }
 
 func main() {
-	node1 := Node{0, nil}
-	node2 := Node{1, []*Node{}}
-	node3 := Node{2, []*Node{&node2}}
-	node4 := Node{3, []*Node{}}
-	node1.other = []*Node{&node2, &node3}
-	node2.other = []*Node{&node4}
-	traverse(&node1)
+	node3 := Node{3, map[rune]*Node{}}
+	node1 := Node{1, map[rune]*Node{'b': &node3}}
+	node2 := Node{2, map[rune]*Node{'a': &node1}}
+	node0 := Node{0, map[rune]*Node{'a': &node1, 'b': &node2}}
+	fmt.Println(traverse(&node0, "bb"))
 }
