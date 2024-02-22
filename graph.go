748c4fc90c4e6f9ecf8c6c4169a2006cc18c78c0
Ashfmate <ashforestunknown@gmail.com>
2024-02-22 22:49:25 +0300

    Made this graph, next is transitioning

--- graph.go
+++ graph.go
@@ -7,13 +7,19 @@ type Node struct {
 	other []*Node
 }
 
+func traverse(start *Node) {
+	for _, val := range start.other {
+		traverse(val)
+	}
+	fmt.Println(start.val)
+}
+
 func main() {
 	node1 := Node{0, nil}
-	node2 := Node{1, []*Node{&node1}}
-	node1.other = []*Node{&node2}
-	cur := &node1
-	for len(cur.other) > 0 {
-		fmt.Println(cur.val)
-		cur = cur.other[0]
-	}
+	node2 := Node{1, []*Node{}}
+	node3 := Node{2, []*Node{&node2}}
+	node4 := Node{3, []*Node{}}
+	node1.other = []*Node{&node2, &node3}
+	node2.other = []*Node{&node4}
+	traverse(&node1)
 }
